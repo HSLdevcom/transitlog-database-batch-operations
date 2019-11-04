0eee6be3cc5eaa6e6e1deb76356ac37aab4a088c
Aki Väliaho <aki.valiaho@hsl.fi>
2019-11-04 10:49:47 +0200

    push container script

---
+++ pushcontainer.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+SYNCMACHINEIP=#FILL
+SYNCMACHINEUSERNAME=#FILL
+SYNCMACHINEPASSWORD=#FILL
+
+function buildDockerContainer {
+	echo "Build succesful, build docker container"
+	docker build -t image .
+	if [ $? -eq 0 ]
+	then
+		echo "Docker image was built"
+		containerHash=`docker images -q image`
+		echo "Container hash: $containerHash"
+	else
+		exit $?
+	fi
+
+}
+
+function saveContainerIntoTar {
+	#Save container into a tar file
+	docker save -o image.tar $containerHash
+	if [ $? -eq 0 ]
+	then
+		echo "Docker save worked"
+		echo "Pushing with scp"
+	else
+		echo "Docker save failed"
+		exit $?
+	fi
+}
+
+function syncContainerWithRSync {
+	echo "Syncing saved container with rsync"
+	sshpass -p $SYNCMACHINEPASSWORD rsync -P -avzhe ssh image.tar $SYNCMACHINEUSERNAME@$SYNCMACHINEIP:image.tar
+}
+
+
+function remoteLoadDockerContainer {
+	echo "Remote loading docker container"
+	sshpass -p $SYNCMACHINEPASSWORD ssh -t $SYNCMACHINEUSERNAME@$SYNCMACHINEIP 'echo '$SYNCMACHINEPASSWORD' | sudo -S docker load -i image.tar'
+}
+
+#Check commands exist
+if [ -z $(command -v sshpass) ]; then sudo apt-get install sshpass; else echo "sshpass is found"; fi
+
+mvn clean install
+
+
+if [ $? -eq 0 ]
+then
+	trap "echo "chowning" &&  chown -R $(logname):$(logname) ../" EXIT ERR INT TERM
+	buildDockerContainer
+	saveContainerIntoTar
+	syncContainerWithRSync
+	remoteLoadDockerContainer
+else
+	echo "Build failed"
+fi
+
+
